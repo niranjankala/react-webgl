47ff60b7555f75c949b796a5fc07c5498b4c6c22
Niranjan Singh <Niranjan.Singh@mind-infotech.com>
2019-06-04 16:19:42 +0530

    -

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  //  state = {};
+  state = {
+    count: 0
+  };
   render() {
     return (
-      <div>
-        <h1>Hello World</h1>
+      <React.Fragment>
+        <span>{this.state.count}</span>
         <button>Increment</button>
-      </div>
+      </React.Fragment>
     );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
