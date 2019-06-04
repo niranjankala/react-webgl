98410f1c49da131806079e417004aba4328cabda
Niranjan Singh <niranjan.singh@d-alchemy.com>
2019-06-04 04:01:24 -0700

    settings stypes on elements--

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,11 +4,19 @@ class Counter extends Component {
   state = {
     count: 0
   };
+  //setting styles inplace
+  styles = {
+    fontSize: 50,
+    fontWeight: "bold"
+  };
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.state.count}</span>
-        <button>Increment</button>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
