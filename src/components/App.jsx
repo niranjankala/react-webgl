e241e25c47797c2466c5c42c235bf0eb8123dc76
Niranjan Singh <Niranjan.Singh@mind-infotech.com>
2019-06-07 18:11:58 +0530

    --

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class App extends Component {
+    state = {}
+    render() {
+        return (<div>
+            <h1>Home</h1>
+        </div>);
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Route, Link, BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
-//import App from "./App";
+import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+
 import Counters from "./components/counters";
 import ThreeBoxScene from "./components/threeboxscene";
 import ThreeOBJLoader from "./components/threeobjloader";
+import BabylonOBJLoader from "./components/babylonobjloader";
+
+const routing = (
+    <Router>
+        <div>
+            <Route path="/" component={App} />
+            <Route path="/threejsobjloader" component={ThreeOBJLoader} />
+            <Route path="/babylonobjloader" component={BabylonOBJLoader} />
+        </div>
+    </Router>
+);
+
 //ReactDOM.render(<Counters />, document.getElementById("root"));
 //ReactDOM.render(<ThreeBoxScene />, document.getElementById("root"));
-ReactDOM.render(<ThreeOBJLoader />, document.getElementById("root"));
+ReactDOM.render(<BabylonOBJLoader />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
